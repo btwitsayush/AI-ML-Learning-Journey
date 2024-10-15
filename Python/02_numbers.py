1c01f6baacbbc98f40028bacb64cacc6b5b6445b
btwitsayush <srivastavaayush6393@gmail.com>
2024-10-15 10:33:19 +0530

    Implemented Arthmetic operations

--- Python/02_numbers.py
+++ Python/02_numbers.py
@@ -3,10 +3,10 @@
 value1=10
 value2=30
 
-# print(value1+value2)
-# print(value1-value2)
-# print(value1/value2)
-# print(value1%value2)
+print(value1+value2)
+print(value1-value2)
+print(value1/value2)
+print(value1%value2)
 
 # If you want to round of thing till 2 decimal
 
